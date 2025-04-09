d367599635865a8fa03d4dfd028499c164b0e25a
mathiasdub <mathias.dbss62@gmail.com>
2025-04-09 17:12:05 +0200

    ajout de précisions sur les models "Individu" et "Elevage" + migrations

--- mysite/elevage/models.py
+++ mysite/elevage/models.py
@@ -3,16 +3,28 @@ from django.db import models
 
 class Elevage(models.Model):
     name = models.CharField(max_length=200)
-    nb_cages = models.IntegerField()
-    qt_nourriture = models.IntegerField()
-    argent = models.IntegerField()
+    nb_cages = models.PositiveIntegerField()
+    qt_nourriture = models.PositiveIntegerField()
+    argent = models.PositiveIntegerField()
     
     def __str__(self):
         return self.name
 
 
 class Individu(models.Model):
-    sexe = models.CharField(max_length=1)        # "m" pour un lapin male et "f" pour un lapin femelle
-    age = models.IntegerField()                  # âge en nombre de mois
-    etat = models.CharField(max_length=10)       # "présent", "vendu", "mort" ou "gravide" 
+    SEXE_CHOICES = [
+        ('m', 'Mâle'),
+        ('f', 'Femelle'),
+    ]
+    
+    ETAT_CHOICES = [
+    ('présent', 'Présent'),
+    ('vendu', 'Vendu'),
+    ('mort', 'Mort'),
+    ('gravide', 'Gravide'),
+    ]
+    
+    sexe = models.CharField(max_length=1, choices=SEXE_CHOICES)        # "m" pour un lapin male et "f" pour un lapin femelle
+    age = models.PositiveIntegerField()                                # âge en nombre de mois
+    etat = models.CharField(max_length=10, choices=ETAT_CHOICES)       # "présent", "vendu", "mort" ou "gravide" 
     elevage = models.ForeignKey(Elevage, on_delete=models.CASCADE, related_name="individus")
