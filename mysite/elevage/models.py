640417eea7cec4cbeb91fa02190bdcf88ae9e0cd
mathiasdub <mathias.dbss62@gmail.com>
2025-04-09 15:35:04 +0200

    ajout du model Individu

--- mysite/elevage/models.py
+++ mysite/elevage/models.py
@@ -11,3 +11,10 @@ class Elevage(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class Individu(models.Model):
+    sexe = models.CharField(max_length=1)        # "m" pour un lapin male et "f" pour un lapin femelle
+    age = models.IntegerField()                  # âge en nombre de mois
+    etat = models.CharField(max_length=10)       # "présent", "vendu", "mort" ou "gravide" 
+    elevage = models.ForeignKey(Elevage, on_delete=models.CASCADE, related_name="individus")
