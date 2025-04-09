97fd362f3e40e5dbde1b35bd631bfe1435cf1c67
mathiasdub <mathias.dbss62@gmail.com>
2025-04-09 14:32:39 +0200

    création du model Elevage et son interface admin

--- mysite/elevage/models.py
+++ mysite/elevage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Elevage(models.Model):
+    name = models.CharField(max_length=200)
+    nb_lapins_m = models.IntegerField()
+    nb_lapins_f = models.IntegerField
+    nb_cages = models.IntegerField()
+    qt_nourriture = models.IntegerField()
+    argent = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
