3458af2dc81462bd2d605338916c3b2e731ff319
mathiasdub <mathias.dbss62@gmail.com>
2025-04-09 15:38:34 +0200

    migrations

--- mysite/elevage/models.py
+++ mysite/elevage/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class Elevage(models.Model):
     name = models.CharField(max_length=200)
-    nb_lapins_m = models.IntegerField()
-    nb_lapins_f = models.IntegerField
     nb_cages = models.IntegerField()
     qt_nourriture = models.IntegerField()
     argent = models.IntegerField()
