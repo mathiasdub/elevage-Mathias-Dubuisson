319c4ccbe4057b11b61601d9b801d4f80179350a
mathiasdub <mathias.dbss62@gmail.com>
2025-04-19 19:41:43 +0200

    ajout de la classe concernant les règles du jeu

--- mysite/elevage/models.py
+++ mysite/elevage/models.py
@@ -28,3 +28,24 @@ class Individu(models.Model):
     age = models.PositiveIntegerField()                                # âge en nombre de mois
     etat = models.CharField(max_length=10, choices=ETAT_CHOICES)       # "présent", "vendu", "mort" ou "gravide" 
     elevage = models.ForeignKey(Elevage, on_delete=models.CASCADE, related_name="individus")
+
+
+class Regle:
+    # Économie
+    PRIX_NOURRITURE_PAR_KG = 1.0       # €
+    PRIX_CAGE = 20.0                  # €
+    PRIX_VENTE_LAPIN = 50.0            # €
+
+    # Nourriture (par jour)
+    CONSOMMATION_MOIS_1 = 0            # Lait maternel
+    CONSOMMATION_MOIS_2 = 0.1          # kg / jour
+    CONSOMMATION_MOIS_3_ET_PLUS = 0.25 # kg / jour
+
+    # Reproduction
+    PORTEE_MAX = 4                     # max lapereaux par mise bas
+    INDIVIDUS_PAR_CAGE_MAX = 6
+    INDIVIDUS_PAR_CAGE_SURPOP = 10
+
+    AGE_MATURITE_GRAVIDITE = 6         # mois
+    AGE_MAX_GRAVIDITE = 48             # mois
+    DUREE_GESTATION = 1                # mois
