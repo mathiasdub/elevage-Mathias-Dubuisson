2d298b85bcd56ac26ae12998945258aeab73c38d
mathiasdub <mathias.dbss62@gmail.com>
2025-04-19 19:32:09 +0200

    ajout d'un form dans la vue detail pour saisir les actions du tour

--- mysite/elevage/forms.py
+++ mysite/elevage/forms.py
@@ -1,18 +1,36 @@
 from django import forms
 from .models import Elevage, Individu
-from django.forms import modelformset_factory
+
 
 class ElevageForm(forms.ModelForm):
     class Meta:
         model = Elevage
         fields = ['name', 'qt_nourriture', 'nb_cages', 'argent']
 
+
 class LapinForm(forms.ModelForm):
     class Meta:
         model = Individu
         fields = ['age', 'sexe', 'etat']
 
-LapinFormSet = modelformset_factory(Individu, form=LapinForm, extra=3)  # On peut ajuster `extra`
 
 class ChoixNombreLapinsForm(forms.Form):
-    nombre_lapins = forms.IntegerField(label="Nombre de lapins", min_value=1, max_value=50)
+    nombre_lapins = forms.IntegerField(label="Nombre de lapins", min_value=0, max_value=50)
+    
+
+class ActionsForm(forms.Form):
+    nourriture_achetee = forms.FloatField(min_value=0, required=True, label="Nourriture achetée (kg)")
+    cages_achetees = forms.IntegerField(min_value=0, required=True, label="Cages achetées")
+    lapins_a_vendre = forms.ModelMultipleChoiceField(
+        queryset=Individu.objects.none(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        label="Lapins à vendre"
+    )
+
+    def __init__(self, *args, **kwargs):
+        elevage = kwargs.pop('elevage', None)
+        super().__init__(*args, **kwargs)
+        # Charger les lapins de l’élevage
+        if elevage:
+           self.fields['lapins_a_vendre'].queryset = elevage.individus.all()
--- mysite/elevage/views.py
+++ mysite/elevage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Elevage, Individu
-from .forms import ElevageForm, LapinForm, ChoixNombreLapinsForm
+from .forms import ElevageForm, LapinForm, ChoixNombreLapinsForm, ActionsForm
 from django.forms import modelformset_factory
 
 
@@ -51,4 +51,42 @@ def liste(request):
 
 def detail(request, elevage_id):
     elevage = get_object_or_404(Elevage, id=elevage_id)
-    return render(request, "elevage/detail.html", {"elevage" : elevage})
+    lapins = elevage.individus.all()
+    
+    if request.method == 'POST':
+        form = ActionsForm(request.POST, elevage=elevage)
+        if form.is_valid():
+            lapins_vendus = form.cleaned_data['lapins_a_vendre']
+            nourriture = form.cleaned_data['nourriture_achetee']
+            cages = form.cleaned_data['cages_achetees']
+
+            prix_nourriture_unitaire = 1  # €/kg
+            prix_cage = 50.0    # € par cage
+            prix_lapin = 100.0  # € reçu par lapin vendu
+
+            revenu = len(lapins_vendus) * prix_lapin
+            coût_total = (nourriture * prix_nourriture_unitaire) + (cages * prix_cage)
+            argent_disponible = elevage.argent + revenu
+
+            if coût_total > argent_disponible:
+                form.add_error(None, "Vous ne pouvez pas acheter autant avec les ressources actuelles.")
+            else:
+                # Mise à jour de l’élevage
+                elevage.qt_nourriture += nourriture
+                elevage.nb_cages += cages
+                elevage.argent = argent_disponible - coût_total
+                elevage.save()
+
+                # Suppression des lapins vendus
+                for lapin in lapins_vendus:
+                    lapin.delete()
+
+                return redirect('elevage:detail', elevage_id=elevage.id)
+    else:
+        form = ActionsForm(elevage=elevage)
+
+    return render(request, 'elevage/detail.html', {
+        'elevage': elevage,
+        'lapins': lapins,
+        'form': form,
+    })
