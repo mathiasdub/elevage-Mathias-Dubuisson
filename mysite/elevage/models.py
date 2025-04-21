d7bcad956f360ca0fdf44741ad886d400e189893
mathiasdub <mathias.dbss62@gmail.com>
2025-04-21 16:16:47 +0200

    améliorations concernant la fonction  'avancer_tour'

--- mysite/elevage/models.py
+++ mysite/elevage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 
 class Elevage(models.Model):
@@ -6,6 +7,81 @@ class Elevage(models.Model):
     nb_cages = models.PositiveIntegerField()
     qt_nourriture = models.PositiveIntegerField()
     argent = models.PositiveIntegerField()
+    
+    def avancer_tour(self, nourriture_achetee, cages_achetees, lapins_vendus):
+        # 1. Met à jour les ressources de l’élevage
+        self.qt_nourriture += nourriture_achetee
+        self.nb_cages += cages_achetees
+        self.argent -= (
+            nourriture_achetee * Regle.PRIX_NOURRITURE_PAR_KG +
+            cages_achetees * Regle.PRIX_CAGE
+        )
+        self.argent += len(lapins_vendus) * Regle.PRIX_VENTE_LAPIN
+
+        for lapin in lapins_vendus:
+            lapin.etat = "vendu"
+            lapin.save()
+
+        # 2. Vieillissement des lapins
+        lapins = self.individus.all()
+        for lapin in lapins:
+            lapin.age += 1
+            lapin.save()
+
+        # 3. Reproduction
+        femelles = self.individus.filter(sexe='f', age__gte=Regle.AGE_MATURITE_GRAVIDITE, age__lte=Regle.AGE_MAX_GRAVIDITE, etat='présent')
+        femelles_gravides = self.individus.filter(etat='gravide')
+        males = self.individus.filter(sexe='m', age__gte=3)  # Mâles adultes
+        
+        # Accouchement des femelles gravides 
+        for femelle in femelles_gravides:
+            nb_bebes = random.randint(1, Regle.PORTEE_MAX)
+            for _ in range(nb_bebes):
+                sexe = random.choice(['f', 'm'])
+                Individu.objects.create(
+                    elevage=self,
+                    age=0,
+                    sexe=sexe,
+                    etat='présent'
+                )
+            femelle.etat = 'présent'  # accouche, redevient normale
+            femelle.save()
+            
+        for femelle in femelles:
+            if random.random() < Regle.PROBA_REPRO: #and males.exists():  # On suppose 50% de chances qu'elle soit fécondée
+                femelle.etat = "gravide"
+                femelle.save()
+
+
+        # 4. Nourriture
+        morts_par_faim = []
+
+        for lapin in self.individus.all():
+            if lapin.age == 0:
+                conso = 0
+            elif lapin.age == 1:
+                conso = Regle.CONSOMMATION_MOIS_2 * 30
+            else:
+                conso = Regle.CONSOMMATION_MOIS_3_ET_PLUS * 30
+
+            if self.qt_nourriture >= conso:
+                self.qt_nourriture -= conso
+            else:
+                morts_par_faim.append(lapin)
+
+        for lapin in morts_par_faim:
+            lapin.etat = "mort"
+            lapin.save()
+
+        # 5. Surpopulation
+        lapins_restants = self.individus.filter(etat__in=['présent', 'gravide'])
+        if lapins_restants.count() > self.nb_cages * Regle.INDIVIDUS_PAR_CAGE_SURPOP:
+            morts = random.sample(list(lapins_restants), k=int(0.5 * lapins_restants.count()))
+            for lapin in morts:
+                lapin.etat = "mort"
+                lapin.save()
+
+        self.save()
     
     def __str__(self):
         return self.name
@@ -28,6 +104,9 @@ class Individu(models.Model):
     age = models.PositiveIntegerField()                                # âge en nombre de mois
     etat = models.CharField(max_length=10, choices=ETAT_CHOICES)       # "présent", "vendu", "mort" ou "gravide" 
     elevage = models.ForeignKey(Elevage, on_delete=models.CASCADE, related_name="individus")
+    
+    def __str__(self):
+        return f"sexe : {self.sexe}, âge : {self.age}, état : {self.etat}"
 
 
 class Regle:
@@ -43,6 +122,7 @@ class Regle:
 
     # Reproduction
     PORTEE_MAX = 4                     # max lapereaux par mise bas
+    PROBA_REPRO = 0.5
     INDIVIDUS_PAR_CAGE_MAX = 6
     INDIVIDUS_PAR_CAGE_SURPOP = 10
 
