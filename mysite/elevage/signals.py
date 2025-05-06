4fdaab2ad049545f8e15b477db79e4ff89649495
Octave CERCLE <octavecercle342@gmail.com>
2025-05-06 10:33:28 +0200

    ajout de signals

--- mysite/elevage/signals.py
+++ mysite/elevage/signals.py
@@ -1,6 +1,12 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,post_migrate
 from django.dispatch import receiver
 from django.contrib.auth.models import User, Group
+
+
+@receiver(post_migrate)
+def create_default_groups(sender, **kwargs):
+    Group.objects.get_or_create(name='basic')
+    Group.objects.get_or_create(name='premium')
 
 @receiver(post_save, sender=User)
 def add_user_to_basic_group(sender, instance, created, **kwargs):
